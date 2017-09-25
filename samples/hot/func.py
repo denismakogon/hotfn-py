895c591fb4476f90dd639c92c93e1c50e548ca50
Denis Makogon <lildee1991@gmail.com>
2017-09-25 17:42:56 +0300

    Updating hot sample

--- samples/hot/func.py
+++ samples/hot/func.py
@@ -1,13 +1,16 @@
 import asyncio
 import sys
+import uvloop
 
 from hotfn.http import request
 from hotfn.http import response
 
 
 class MyProtocol(asyncio.Protocol):
+
     def connection_made(self, transport):
         print('pipe opened', file=sys.stderr, flush=True)
+        super(MyProtocol, self).connection_made(transport=transport)
 
     def data_received(self, data):
         print('received: {!r}'.format(data), file=sys.stderr, flush=True)
@@ -20,16 +23,20 @@ class MyProtocol(asyncio.Protocol):
             http_version, 200, "OK",
             response_data=req_data)
         print(rs.dump(), file=sys.stdout, flush=True)
+        super(MyProtocol, self).data_received(data)
 
     def connection_lost(self, exc):
         print('pipe closed', file=sys.stderr, flush=True)
+        super(MyProtocol, self).connection_lost(exc)
 
 
 if __name__ == "__main__":
-    loop = asyncio.SelectorEventLoop()
     with open("/dev/stdin", "rb", buffering=0) as stdin:
-        loop.run_until_complete(
-            loop.connect_read_pipe(
-                MyProtocol, stdin))
-        loop.run_forever()
-        loop.close()
+        asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+        loop = asyncio.get_event_loop()
+        try:
+            stdin_pipe_reader = loop.connect_read_pipe(MyProtocol, stdin)
+            loop.run_until_complete(stdin_pipe_reader)
+            loop.run_forever()
+        finally:
+            loop.close()
